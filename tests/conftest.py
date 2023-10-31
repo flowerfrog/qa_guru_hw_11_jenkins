3d7a63c59fde4e3dec1e24b87c02f2e0e8f75a7b
flowerfrog <alexosenn@gmail.com>
2023-10-31 14:18:08 +0300

    Refactored conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,36 @@
 import pytest
-from selene.support.shared import browser
+from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
 
 
-@pytest.fixture(scope="function", autouse=True)
-def open_browser():
-    browser.config.base_url = 'https://demoqa.com'
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.driver = driver
+    browser.config.timeout = 2.0
     browser.config.window_width = 1920
-    browser.config.window_height = 1200
-    browser.config.timeout = 2
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless=new')
-    browser.config.driver_options = driver_options
-    yield
+    browser.config.window_height = 1080
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
