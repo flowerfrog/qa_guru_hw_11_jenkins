7a4509283c6e53c0ef3a72ccec035c7f1a08b239
flowerfrog <alexosenn@gmail.com>
2023-10-31 14:19:48 +0300

    Refactored conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,6 @@ import pytest
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-
 from utils import attach
 
 
